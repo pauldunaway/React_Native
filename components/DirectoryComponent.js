feaa1160fa6e3e2122852b5cf53cbda1cd929f2b
Paul DUnaway <pauldunaway@yahoo.com>
2020-11-18 11:31:02 -0500

    Components Part 2

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import React from "react";
+import { FlatList } from "react-native";
+import { ListItem } from "react-native-elements";
 
 function Directory(props) {
-
-    const renderDirectoryItem = ({item}) => {
+    const renderDirectoryItem = ({ item }) => {
         return (
             <ListItem
                 title={item.name}
                 subtitle={item.description}
                 onPress={() => props.onPress(item.id)}
-                leftAvatar={{ source: require('./images/react-lake.jpg')}}
+                leftAvatar={{ source: require("./images/react-lake.jpg") }}
             />
         );
     };
@@ -19,7 +18,7 @@ function Directory(props) {
         <FlatList
             data={props.campsites}
             renderItem={renderDirectoryItem}
-            keyExtractor={item => item.id.toString()}
+            keyExtractor={(item) => item.id.toString()}
         />
     );
 }
