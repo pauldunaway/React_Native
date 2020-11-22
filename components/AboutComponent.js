bcbc428dd1a5be67b3a35d6be1131c33b4fb3b61
Paul DUnaway <pauldunaway@yahoo.com>
2020-11-22 16:48:56 -0500

    Icon, Favorites, and Comments

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -5,17 +5,19 @@ import { ScrollView, Text } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import { Card, ListItem } from "react-native-elements";
 
-export function Mission() {
+function Mission() {
     return (
-        <Text>
-            We present a curated database of the best campsites in the vast
-            woods and backcountry of the World Wide Web Wilderness. We increase
-            access to adventure for the public while promoting safe and
-            respectful use of resources. The expert wilderness trekkers on our
-            staff personally verify each campsite to make sure that they are up
-            to our standards. We also present a platform for campers to share
-            reviews on campsites they have visited with each other.
-        </Text>
+        <Card title="Our Mission">
+            <Text>
+                We present a curated database of the best campsites in the vast
+                woods and backcountry of the World Wide Web Wilderness. We increase
+                access to adventure for the public while promoting safe and
+                respectful use of resources. The expert wilderness trekkers on our
+                staff personally verify each campsite to make sure that they are up
+                to our standards. We also present a platform for campers to share
+                reviews on campsites they have visited with each other.
+            </Text>
+        </Card>
     );
 }
 
@@ -47,9 +49,7 @@ class About extends Component {
 
         return (
             <ScrollView>
-                <Card title="Our Mission">
-                    <Mission />
-                </Card>
+                <Mission />
                 <Card title="Community Partners">
                     <FlatList
                         data={this.state.partners}
